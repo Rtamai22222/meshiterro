d2f572aafaccaed070edd569ca58f4eab9d99946
EC2 Default User <ec2-user@ip-172-31-20-212.ap-northeast-1.compute.internal>
2022-12-13 05:49:47 +0000

    [Finish] 4-10

--- app/controllers/post_images_controller.rb
+++ app/controllers/post_images_controller.rb
@@ -1,5 +1,14 @@
 class PostImagesController < ApplicationController
+  
   def new
+    @post_image = PostImage.new
+  end
+  
+  def create
+    @post_image = PostImage.new(post_image_params)
+    @post_image.user_id = current_user.id #current_userはdeviseのヘルパーメソッド。ログイン中のユーザーの情報を取得できる。current_user.XX のように扱う.id //.name //.emailなど
+    @post_image.save
+    redirect_to post_images_path
   end
 
   def index
@@ -7,4 +16,17 @@ class PostImagesController < ApplicationController
 
   def show
   end
+  
+  def destroy
+  end
+  
+  
+  private
+  
+  
+  def post_image_params
+    params.require(:post_image).permit(:shop_name, :image, :caption)
+  end
+  
+  
 end
